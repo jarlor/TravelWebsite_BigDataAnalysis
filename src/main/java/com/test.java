56fb9c004aa1478661e3d3fc6857abfbc7786be0
GongChangJL <zjl58960902@outlook.com>
2021-04-27 12:46:00 +0800

    新建分支——封装

---
+++ src/main/java/com/test.java
@@ -0,0 +1,15 @@
+package com;
+
+import Connected.*;
+
+/**
+ * @author GCJL
+ * @date 2021/4/8 22:19
+ */
+public class test {
+    public static void main(String[] args) throws Exception {
+        HDFS hdfs = new HDFS();
+
+
+    }
+}
