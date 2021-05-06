34076801022a0197b5065d721d6d24c22977fc42
chang <2269673232@qq.com>
2021-05-06 13:57:24 +0800

    新增统计酒店中评论词频单词

--- src/main/java/DataProcess/CountWord/Run.java
+++ src/main/java/DataProcess/CountWord/Run.java
@@ -7,9 +7,7 @@ import org.apache.hadoop.util.ToolRunner;
  * @author GCJL
  * @date 2021/4/28 14:18
  */
-public class
-
-Run {
+public class Run {
     public static void main(String[] args) throws Exception {
         ToolRunner.run(new CWTool(),args);
     }
--- src/main/java/DataProcess/CountWord/reducer/CWReducer.java
+++ src/main/java/DataProcess/CountWord/reducer/CWReducer.java
@@ -5,7 +5,6 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
@@ -16,7 +15,20 @@ import java.io.IOException;
  * @date 2021/4/28 14:25
  */
 public class CWReducer extends TableReducer<Text, IntWritable, ImmutableBytesWritable> {
+
+    private static byte[] family =  "word_info".getBytes();
+    private static byte[] column = "count".getBytes();
+
     @Override
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        Put put = new Put(Bytes.toBytes(key.toString()));
+        put.addColumn(family,column,Bytes.toBytes(sum));
+        context.write(null,put);
     }
+
 }
